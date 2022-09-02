9586fefef18fa9686b54aeb8c28cdb80f7d27b24
Tanbir Sakib <iamtsakib@gmail.com>
2022-09-02 20:42:22 +0600

    creating the ds bootcamp repo

---
+++ problems/diagram.py
@@ -0,0 +1,10 @@
+for i in range(1,7):
+    for j in range(1, i+1):
+        print('*', end='')
+    print('\n')
+    
+for i in range(1,7):
+    for j in range(1, 7-i):
+        print('*', end='')
+    print('\n')
+    
